297cb4fc76191bfecad934ddc93fe1e0ee256536
btelenchana <95003305+btelenchana@users.noreply.github.com>
2021-11-29 18:00:15 -0500

    Add files via upload

---
+++ Ejercicio1_Parte1_BT.py
@@ -0,0 +1,24 @@
+print('Empezando a trabajar con Python\n') #muestra leyenda y 2 saltos
+print('Realizado por: Braulio Telenchana\n\n') #muestra leyenda y 2 saltos
+print('Consultando los tipos de valores:\n\n') #muestra leyenda y 2 saltos
+print('El tipo de dato de 875 es:\n') #muestra leyenda y 1 salto
+print(type(875)) #muestra el tipo de dato
+print('\n\n') #muestra 2 saltos
+print('El tipo de dato de 4.89 es:\n') #muestra leyenda y 1 salto 
+print(type(4.89)) #muestra el tipo de dato
+print('\n\n') #muestra 2 saltos
+print('El tipo de dato del texto: Now is better than never. es:\n')  #muestra leyenda y 1 salto
+print(type('Now is better than never.'))  #muestra el tipo de dato
+print('\n\n') #muestra 2 saltos
+print('El tipo de dato de 1.32 es:\n')  #muestra leyenda y 1 salto
+print(type(1.32))  #muestra el tipo de dato
+print('\n\n') #muestra 2 saltos
+print('¿El valor 5 + 8i corresponde a un valor entero?\n')  #muestra leyenda y 1 salto
+print(isinstance(5+8j,int))  #valida si el dato es entero
+print('\n\n') #muestra 2 saltos
+print('¿El valor 8.2 corresponde a un valor entero?\n') #muestra leyenda y 1 salto
+print(isinstance(8.2,int)) #valida si el dato es entero
+print('\n\n') #muestra 2 saltos
+print('¿El texto: Readability counts. corresponde a un string?\n') #muestra leyenda y 1 salto
+print(isinstance('Readability counts.',str)) #valida si el dato es string
+print('\n\n') #muestra 2 saltos
---
+++ Ejercicio1_Parte2_BT.py
@@ -0,0 +1,25 @@
+print('Programa que identifica el tipo de dato de un valor ingresado por el usuario, se realizarán cinco interacciones:\n\n')
+dato=input('Primera Interacción, ingrese un valor cualquiera: ') #ingreso de valor en una variable
+print('Este tipo de dato en Python es:') #muestra mensaje
+print(type(dato)) #muestra tipo de dato
+print('')
+print('')
+dato=input('Segunda Interacción, ingrese un valor cualquiera: ') #ingreso de valor en una variable
+print('Este tipo de dato en Python es:') #muestra mensaje
+print(type(dato)) #muestra tipo de dato
+print('')
+print('')
+dato=input('Tercera Interacción, ingrese un valor cualquiera: ') #ingreso de valor en una variable
+print('Este tipo de dato en Python es:') #muestra mensaje
+print(type(dato)) #muestra tipo de dato
+print('')
+print('')
+dato=input('Cuarta Interacción, ingrese un valor cualquiera: ') #ingreso de valor en una variable
+print('Este tipo de dato en Python es:') #muestra mensaje
+print(type(dato)) #muestra tipo de dato
+print('')
+print('')
+dato=input('Quinta Interacción, ingrese un valor cualquiera: ') #ingreso de valor en una variable
+print('Este tipo de dato en Python es:') #muestra mensaje
+print(type(dato)) #muestra tipo de dato
+print('¡YA NO SE HARÁN MÁS INTERACCIONES!') #muestra mensaje
